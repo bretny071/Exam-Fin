8cf1951440d11aec4ad225e002e4999b55748b8d
Bretny Bal-ut <abonbretny@gmail.com>
2025-05-25 10:12:37 +0800

    mdArray.js

---
+++ mdArray.js
@@ -0,0 +1,13 @@
+let subArray1 = ["Genevieve", "Juan", "Luna", "Gabriel", "Elise"];
+let subArray2 = [24, 65, 21, 5, 9];
+
+let restructuredArray = [];
+
+for (let i = 0; i < subArray1.length; i++) {
+    restructuredArray.push([subArray1[i], subArray2[i]]);
+}
+
+console.log("Restructured Multi-Dimensional Array:");
+restructuredArray.forEach(item => {
+    console.log(item);
+});
